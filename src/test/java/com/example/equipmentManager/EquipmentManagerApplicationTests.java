5b53996ab1369f76c66eb15f02420dcc74d3dd19
Ahmed Rekibi <ahmedrekibi158@gmail.com>
2025-05-08 09:08:30 +0100

    First spring boot project commit

---
+++ src/test/java/com/example/equipmentManager/EquipmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.equipmentManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
